80bf327ea0301604bac170e61baedebdd426d50e
taboulet33 <edouard.thieuleux@gmail.com>
2011-01-23 18:06:08 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edouard::Application.config.secret_token = 'f48f32229c468484e9a93300b59621d0af9d6710798c51a8a7e6f9765055c94be784e8553891773abd07c20d888106c806b84a64580a15e0578fc019804e6bea'
